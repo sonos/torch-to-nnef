30eb24205f7dbc1e2945840e425b8220922ac2c7
Julien Balian <julien.balian@sonos.com>
2022-03-04 15:31:52 +0100

    feat: all basic activations works

--- torch_to_nnef/op/primitive.py
+++ torch_to_nnef/op/primitive.py
@@ -2,6 +2,7 @@ import typing as T
 from nnef_tools.model import Operation as NOperation
 from nnef_tools.model import Tensor as NTensor
 import torch
+import numpy as np
 
 from .base import _torch_to_nnef_typestr
 
@@ -34,6 +35,7 @@ def _unary_op(nnef_op_type: str, g, node, name_to_tensor, null_ref):
         shape=node.tensor_size,
     )
     name_to_tensor[node.export_name] = out
+
     outputs = [out]
     NOperation(
         graph=g,
@@ -48,8 +50,106 @@ def _unary_op(nnef_op_type: str, g, node, name_to_tensor, null_ref):
     )
 
 
+def _unary_op_with_constants(nnef_op_type, torch_graph, **kwargs):
+
+    g = kwargs["g"]
+    node = kwargs["node"]
+    name_to_tensor = kwargs["name_to_tensor"]
+
+    for const_node_name in node.export_inputs[1:]:
+        const = torch_graph.get_node_by_export_name(const_node_name)
+        if const.dtype == "Tensor":
+            # is a NodeState
+
+            # for now
+            # in such case we inline tensor data values in graph def so it
+            # should be kept small.
+
+            # we might want to implement variable instead if too big
+            data = const.data.numpy()
+            nptype = data.dtype.type
+        else:
+            nptype = _torch_to_nnef_typestr(const.subtype or const.dtype)
+            data = np.array(const.value, dtype=nptype)
+        name_to_tensor[const_node_name] = NTensor(
+            g,
+            const.export_name,
+            data=data,
+            dtype=nptype,
+            shape=const.tensor_size,
+        )
+    return _unary_op(nnef_op_type, **kwargs)
+
+
 def relu(torch_graph, **kwargs):
     return _unary_op("relu", **kwargs)
+
+
+def sigmoid(torch_graph, **kwargs):
+    return _unary_op("sigmoid", **kwargs)
+
+
+def tanh(torch_graph, **kwargs):
+    return _unary_op("tanh", **kwargs)
+
+
+def softmax(**kwargs):
+    node = kwargs['node']
+    if node.inputs[2]:
+        del node.inputs[2]
+    return _unary_op_with_constants("softmax", **kwargs)
+
+
+def softplus(torch_graph, **kwargs):
+    """
+    Note: numerical stability applied in Pytorch is not done in NNEF vanilla
+    implementation, nor case beta != 1.
+
+    Pytorch ref:
+        y = (1/beta) * log(exp(beta * x) + 1)  if ((beta * x) < thresh) else x
+    NNEF ref:
+        y = log(exp(x) + 1.0)
+
+    """
+    node = kwargs['node']
+    const = torch_graph.get_node_by_export_name(node.export_inputs[1])
+    if const.value != 1:
+        raise NotImplemented(
+            "This version is not implemented and"
+            " would need use of a specific fragment"
+        )
+    node.inputs = node.inputs[:1]
+    return _unary_op("softplus", **kwargs)
+
+
+def elu(**kwargs):
+    node = kwargs['node']
+    node.inputs = node.inputs[:2]  # remove inplace param
+    return _unary_op_with_constants("elu", **kwargs)
+
+
+def leaky_relu(**kwargs):
+    node = kwargs['node']
+    node.inputs = node.inputs[:2]  # remove inplace param
+    return _unary_op_with_constants("leaky_relu", **kwargs)
+
+
+def prelu(**kwargs):
+    node = kwargs['node']
+    node.inputs = node.inputs[:2]  # remove inplace param
+    return _unary_op_with_constants("prelu", **kwargs)
+
+
+def gelu(torch_graph, **kwargs):
+    return _unary_op("gelu", **kwargs)
+
+
+def selu(**kwargs):
+    return _unary_op_with_constants("selu", **kwargs)
+
+
+def silu(**kwargs):
+    return _unary_op_with_constants("silu", **kwargs)
 
 
 def _convolution(g, node, name_to_tensor, null_ref, torch_graph):
