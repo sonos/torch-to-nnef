b6e8feb4bb4a277b1519826ce7cc18eae5021c1f
gith <julien.balian@sonos.com>
2022-03-17 12:14:14 +0100

    feat: chore extract tract related fn in dedicated module

--- tests/test_simple_model.py
+++ tests/test_simple_model.py
@@ -14,6 +14,11 @@ from torch import nn
 from torchvision import models as vision_mdl
 
 from torch_to_nnef.export import export_model_to_nnef
+from torch_to_nnef.tract import (
+    build_io,
+    debug_dumper_pytorch_to_onnx_to_nnef,
+    tract_assert_io,
+)
 
 INPUT_AND_MODELS = []
 
@@ -368,22 +373,6 @@ if os.environ.get("Q8"):
     ]
 
 
-def tract_convert_onnx_to_nnef(onnx_path, io_npz_path, nnef_path):
-    subprocess.check_call(
-        f'tract {onnx_path} --input-bundle {io_npz_path}  dump --nnef {nnef_path}',
-        shell=True,
-    )
-
-
-def tract_assert_io(nnef_path: Path, io_npz_path: Path):
-    cmd = f"tract {nnef_path} --input-bundle {io_npz_path} -O run --assert-output-bundle {io_npz_path}"
-    try:
-        subprocess.check_call(cmd, shell=True, stderr=subprocess.DEVNULL)
-        return True
-    except subprocess.CalledProcessError:
-        return False
-
-
 def test_should_fail_since_false_output():
     with tempfile.TemporaryDirectory() as tmpdir:
         test_input = torch.rand(1, 10, 100)
@@ -412,15 +401,12 @@ def test_should_fail_since_false_output():
         ), f"SHOULD fail tract io check with {model}"
 
 
-def nop(x, *args, **kwargs):
-    return x
-
-
-def special_quantize_io(x, model, is_input):
-    if is_input:
-        # return model.quantize()x
-        return model.quant(x).int_repr()
-    return model.quant(x).int_repr()
+# INPUT_AND_MODELS = [
+# (torch.rand(1, 10, 100), layer)
+# for layer in [
+# nn.LSTM(100, 5),
+# ]
+# ]
 
 
 @pytest.mark.parametrize("test_input,model", INPUT_AND_MODELS)
@@ -432,15 +418,9 @@ def test_model_export(test_input, model):
 
         model = model.eval()
 
-        tup_inputs = (
-            test_input if isinstance(test_input, tuple) else (test_input,)
+        input_names, output_names = build_io(
+            model, test_input, io_npz_path=io_npz_path
         )
-        input_names = [f"input_{idx}" for idx, _ in enumerate(tup_inputs)]
-        test_output = model(*tup_inputs)
-        if isinstance(test_output, torch.Tensor):
-            test_output = [test_output]
-        test_output = list(test_output)
-        output_names = [f"output_{idx}" for idx, _ in enumerate(test_output)]
         export_model_to_nnef(
             model=model,
             args=test_input,
@@ -449,22 +429,6 @@ def test_model_export(test_input, model):
             output_names=output_names,
             verbose=False,
         )
-        if isinstance(model, torch.quantization.QuantWrapper):
-            fn = partial(special_quantize_io, model=model)
-        else:
-            fn = nop
-        kwargs = {
-            f"input_{idx}": fn(input_arg.detach(), is_input=True).numpy()
-            for idx, input_arg in enumerate(tup_inputs)
-        }
-        kwargs.update(
-            {
-                f"output_{idx}": fn(output_arg.detach(), is_input=True).numpy()
-                for idx, output_arg in enumerate(test_output)
-            }
-        )
-
-        np.savez(io_npz_path, **kwargs)
         real_export_path = export_path.with_suffix(".nnef.tgz")
         assert real_export_path.exists()
         try:
@@ -486,23 +450,7 @@ def test_model_export(test_input, model):
                 f"&& tar -xvzf {real_export_path} && cp {io_npz_path} {exp_path}/io.npz",
                 shell=True,
             )
-            tract_exp_path = exp_path / "tract"
-            tract_exp_path.mkdir()
-            onnx_path = tract_exp_path / "model.onnx"
-            torch.onnx.export(
-                model,
-                test_input,
-                str(onnx_path),
-                input_names=input_names,
-                output_names=output_names,
-            )
-            nnef_path = tract_exp_path / "tract_onnx_converted_model.nnef"
-            tract_convert_onnx_to_nnef(
-                onnx_path,
-                io_npz_path,
-                nnef_path=nnef_path,
-            )
-            subprocess.check_output(
-                f"cd {tract_exp_path} && tar -xvf {nnef_path}", shell=True
+            debug_dumper_pytorch_to_onnx_to_nnef(
+                model, test_input, target_folder=exp_path / "tract"
             )
             raise exp
