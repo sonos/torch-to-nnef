08ab3363ef0d060fd4db9c9c5147d3a4645c3702
Julien Balian <julien.balian@sonos.com>
2022-03-19 12:52:17 +0100

    fix: nicer fragment lib loading

---
+++ torch_to_nnef/op/fragment/__init__.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+EXTENSION = ".nnef"
+FRAGMENTS = {}
+for path in Path(__file__).parent.glob(f"**/*{EXTENSION}"):
+    if path.is_file():
+        if path.name in FRAGMENTS:
+            raise KeyError(f"This fragments name: {path.name} is already used")
+        with path.open("r", encoding="utf8") as fh:
+            FRAGMENTS[path.with_suffix("").name] = fh.read().strip()
