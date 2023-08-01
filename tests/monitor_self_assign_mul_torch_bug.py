0f7f092e6a868f8154faede7d5c53f43fb0f7e13
Julien Balian <julien.balian@sonos.com>
2023-08-01 10:42:47 +0200

    fix: disable fft tests that need feature gate on tract enabled

---
+++ tests/monitor_self_assign_mul_torch_bug.py
@@ -0,0 +1,27 @@
+"""Monitor failure of torch JIT regarding self assign mul"""
+
+import typing
+
+import torch
+
+from tests.utils import check_model_io_test
+
+
+class NetWrapper(torch.nn.Module):
+    def __init__(
+        self,
+        alpha: typing.Optional[float] = None,
+    ):
+        super().__init__()
+        self.alpha = alpha
+
+    def forward(self, inp):
+        if self.alpha is not None:
+            inp *= self.alpha
+        return inp
+
+
+if __name__ == "__main__":
+    check_model_io_test(
+        model=NetWrapper(3.0), test_input=torch.arange(4).reshape(2, 2).float()
+    )
