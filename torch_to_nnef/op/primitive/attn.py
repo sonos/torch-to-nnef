acbf93e5c888f1b3349e15ab6578cbac67ed44cd
Julien Balian <julien.balian@sonos.com>
2023-09-05 17:50:48 +0200

    fix: more scaled_dot_product_attention variants support

--- torch_to_nnef/op/primitive/attn.py
+++ torch_to_nnef/op/primitive/attn.py
@@ -8,6 +8,7 @@ from torch_to_nnef.op.primitive.base import (
     add_single_output_op,
     get_or_add_tensor_variable_in_nnef,
 )
+from torch_to_nnef.torch_graph.ir_data import PythonConstant
 
 OP_REGISTRY = AtenOpRegistry()
 
@@ -34,10 +35,6 @@ def scaled_dot_product_attention(g, node, name_to_tensor, **kwargs):
         raise TorchToNNEFNotImplementedError(
             "scaled_dot_product_attention with > 0 dropout_p not implemented"
         )
-    if attn_mask_node.dtype != torch.float32:
-        raise TorchToNNEFNotImplementedError(
-            "scaled_dot_product_attention with attn_mask_node non float not implemented"
-        )
 
     query_tensor = get_or_add_tensor_variable_in_nnef(
         g, query_node, name_to_tensor
@@ -46,26 +43,40 @@ def scaled_dot_product_attention(g, node, name_to_tensor, **kwargs):
     value_tensor = get_or_add_tensor_variable_in_nnef(
         g, value_node, name_to_tensor
     )
+
+    fragment_suffix_id = ""
+    has_masked_attn = not isinstance(attn_mask_node, PythonConstant)
+
+    inputs = [query_tensor, key_tensor, value_tensor]
+    if has_masked_attn:
+        if attn_mask_node.dtype != torch.float32:
+            raise TorchToNNEFNotImplementedError(
+                "scaled_dot_product_attention with attn_mask_node non float not implemented"
+            )
+        attn_mask_tensor = get_or_add_tensor_variable_in_nnef(
+            g, attn_mask_node, name_to_tensor
+        )
+        inputs.append(attn_mask_tensor)
+    else:
+        assert attn_mask_node.data is None
+        fragment_suffix_id = "_nomask"
+
     fragment_name = ""
     if key_node.rank == 3:
-        fragment_name = "scaled_dot_product_attention_3d"
+        fragment_name = f"scaled_dot_product_attention_3d{fragment_suffix_id}"
     elif key_node.rank == 4:
-        fragment_name = "scaled_dot_product_attention_4d"
+        fragment_name = f"scaled_dot_product_attention_4d{fragment_suffix_id}"
     else:
         raise TorchToNNEFNotImplementedError(
             "shape unexpected for scaled_dot_product_attention"
         )
-
-    attn_mask_tensor = get_or_add_tensor_variable_in_nnef(
-        g, attn_mask_node, name_to_tensor
-    )
 
     add_single_output_op(
         g,
         node,
         name_to_tensor,
         fragment_name,
-        inputs=(query_tensor, key_tensor, value_tensor, attn_mask_tensor),
+        inputs=tuple(inputs),
         # attrs={"axes": [pick_rank(input_node, dim) for dim in axes_node.data]},
     )
 
