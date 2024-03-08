60972f96add0be46c93a9a0166b66b4804960a83
Julien Balian <julien.balian@sonos.com>
2024-03-08 11:45:50 +0100

    feat: add custom extractor submodules
    fix: wrong import name
    
    fix: missing customops_kind in __init__

---
+++ torch_to_nnef/op/custom_extractors/__init__.py
@@ -0,0 +1,31 @@
+"""
+`op.custom_extractors` provide mechanism to extract to NNEF while bypassing full expension
+of `torch.Module` within `torch_graph` which by default use torch.jit.trace .
+
+This may be for two main reasons:
+    - Some layer such as LSTM/GRU have complex expension which are better
+      handled by encapsulation instead of spreading high number of variable
+    - Some layer might not be serializable to .jit
+    - There might be some edge case where you prefer to keep full control on
+      exported NNEF subgraph.
+
+"""
+from torch_to_nnef.op.custom_extractors.base import (
+    CUSTOMOP_KIND,
+    ModuleInfoExtractor,
+)
+
+# load default custom registries
+from torch_to_nnef.op.custom_extractors.rnn import (
+    GRUExtractor,
+    LSTMExtractor,
+    RNNExtractor,
+)
+
+__all__ = (
+    "CUSTOMOP_KIND",
+    "ModuleInfoExtractor",
+    "RNNExtractor",
+    "LSTMExtractor",
+    "GRUExtractor",
+)
---
+++ torch_to_nnef/op/custom_extractors/base.py
@@ -0,0 +1,175 @@
+import typing as T
+
+import torch
+from torch import nn
+
+from torch_to_nnef.exceptions import (
+    NotFoundModuleExtractor,
+    TorchToNNEFNotImplementedError,
+)
+
+CUSTOMOP_KIND = "wired_custom::"
+
+
+class _ModuleInfoRegistery(type):
+
+    """Allow extract in NNEF behavior from specific nn.Module"""
+
+    MODULE_CLASS: T.Optional[T.Type[nn.Module]] = None
+
+    REGISTRY: T.Dict[T.Type[nn.Module], "_ModuleInfoRegistery"] = {}
+
+    def __new__(cls, name, bases, attrs):
+        # instantiate a new type corresponding to the type of class being defined
+        # this is currently RegisterBase but in child classes will be the child class
+        new_cls = type.__new__(cls, name, bases, attrs)
+        if new_cls.MODULE_CLASS is not None:
+            cls.REGISTRY[new_cls.MODULE_CLASS] = new_cls
+        return new_cls
+
+    @classmethod
+    def get_registry(cls):
+        return dict(cls.REGISTRY)
+
+
+class ModuleInfoExtractor(metaclass=_ModuleInfoRegistery):
+    MODULE_CLASS: T.Optional[T.Type[nn.Module]] = None
+
+    def __init__(self):
+        if self.MODULE_CLASS is None:
+            raise TorchToNNEFNotImplementedError(
+                f"Need to specify MODULE_CLASS in class {self.__class__}"
+            )
+
+    @classmethod
+    def get_by_kind(cls, kind: str):
+        classname = kind.replace(CUSTOMOP_KIND, "")
+        extractor_cls = {
+            str(k.__name__): v for k, v in cls.get_registry().items()
+        }.get(classname)
+        if extractor_cls is not None:
+            return extractor_cls()
+        raise NotFoundModuleExtractor(classname)
+
+    @classmethod
+    def get_by_module(cls, module: nn.Module):
+        extractor_cls = cls.get_registry().get(module.__class__)
+        if extractor_cls is not None:
+            return extractor_cls()
+        raise NotFoundModuleExtractor(module.__class__)
+
+    def generate_in_torch_graph(self, torch_graph, *args, **kwargs):
+        # ensure empty at first
+        assert torch_graph.inputs == []
+        assert torch_graph.data_nodes == []
+        assert torch_graph.op_nodes == []
+        assert torch_graph.outputs == []
+        self._generate_in_torch_graph(torch_graph, *args, **kwargs)
+        # ensure correctly populated graph
+        assert torch_graph.inputs
+        assert torch_graph.data_nodes
+        assert torch_graph.op_nodes
+        assert torch_graph.outputs
+
+    @property
+    def _cname_slug(self) -> str:
+        if self.MODULE_CLASS:
+            return self.MODULE_CLASS.__name__
+        return "NotSetted"
+
+    @staticmethod
+    def _call_original_mod_with_args(mod, *args):
+        """Allow to reformat args in sub class.
+
+        ie: in LSTM there is a difference between
+            - jit lstm with flat arguments tensors
+            - LSTM python interface with states tensors in a tuple
+        """
+        return mod(*args)
+
+    def _generate_in_torch_graph(
+        self, torch_graph, provided_inputs, provided_outputs
+    ):
+        # pylint: disable-next=import-outside-toplevel
+        from torch_to_nnef import torch_graph as tg
+
+        inputs = []
+        for idx, arg in enumerate(torch_graph.tracer.args):
+            if provided_inputs:
+                tensor_variable = provided_inputs[idx]
+            else:
+                if arg is None:
+                    continue
+                tensor_variable = tg.TensorVariable(
+                    name=f"{self._cname_slug}_input_{idx}",
+                    shape=list(arg.shape),
+                    dtype=arg.dtype,
+                    quant=None,  # would probably need better handling
+                    data=None,
+                )
+            inputs.append(tensor_variable)
+        # in case of rnn 2nd parameter is a tuple of states
+        results = self._call_original_mod_with_args(
+            torch_graph.tracer.mod, *torch_graph.tracer.args
+        )
+
+        if isinstance(results, torch.Tensor):
+            results = (results,)
+
+        expanded_results = []
+        for result in results:
+            if isinstance(result, (tuple, list)):
+                for sub_result in result:
+                    expanded_results.append(sub_result)
+            else:
+                expanded_results.append(result)
+
+        outputs = []
+        # ugly hack in case provided multi output operators with TupleTensors packing
+        if (
+            provided_outputs is not None
+            and isinstance(
+                provided_outputs[0],
+                tg.ir_data.TupleTensors,
+            )
+            and len(provided_outputs) == 1
+        ):
+            provided_outputs = provided_outputs[0].data
+        for idx, result in enumerate(expanded_results):
+            if provided_outputs and idx < len(provided_outputs):
+                tensor_variable = provided_outputs[idx]
+            else:
+                tensor_variable = tg.TensorVariable(
+                    name=f"{self._cname_slug}_output_{idx}",
+                    shape=list(result.shape),
+                    dtype=result.dtype,
+                    quant=None,  # would probably need better handling
+                    data=None,
+                )
+            outputs.append(tensor_variable)
+        torch_graph.inputs = inputs
+        torch_graph.outputs = outputs
+        torch_graph.data_nodes = inputs + outputs
+        torch_graph.op_nodes.append(
+            tg.TorchOp(
+                kind=f"{CUSTOMOP_KIND}{self._cname_slug}",
+                inputs=inputs,
+                outputs=outputs,
+                op_ref=torch_graph.tracer.mod,
+                call_name=self._cname_slug,
+                module_path="",
+                scope="",
+            )
+        )
+
+    def convert_to_nnef(
+        self,
+        g,
+        node,
+        name_to_tensor,
+        null_ref,
+        torch_graph,
+        nnef_spec_strict: bool,
+        **kwargs,
+    ):
+        raise TorchToNNEFNotImplementedError()
