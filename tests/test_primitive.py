e7feee446d17b61e73450c488942daaa3a18b3e0
Julien Balian <julien.balian@sonos.com>
2024-04-04 16:02:50 +0200

    fix: reactivate previous slices test

--- tests/test_primitive.py
+++ tests/test_primitive.py
@@ -414,8 +414,8 @@ INPUT_AND_MODELS += [
     for layer in [
         # test slice
         UnaryPrimitive(lambda x: x[:, 2:, :]),
-        # UnaryPrimitive(lambda x: x[..., 1::2]),
-        # UnaryPrimitive(lambda x: x[..., :2, 1::2]),
+        UnaryPrimitive(lambda x: x[..., 1::2]),
+        UnaryPrimitive(lambda x: x[..., :2, 1::2]),
         torch.nn.LayerNorm(10),
         torch.nn.LayerNorm((3, 10), eps=1e-5, elementwise_affine=True),
         torch.nn.GLU(),
