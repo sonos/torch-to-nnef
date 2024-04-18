d0cfcc467c11a5a216ff1ed503d1a57f3d83e098
Julien Balian <julien.balian@sonos.com>
2024-04-18 16:37:07 +0200

    fix: export llama2 to use different non caching rotary embedding

--- scripts/export_llama2.py
+++ scripts/export_llama2.py
@@ -1,5 +1,6 @@
 import argparse
 import logging as log
+import os
 from enum import Enum
 from pathlib import Path
 
@@ -9,15 +10,57 @@ from transformers import (  # LlamaConfig,; LlamaModel,; LlamaTokenizer,
     AutoTokenizer,
     LlamaForCausalLM,
 )
+from transformers.models.llama import modeling_llama
 
 from torch_to_nnef.export import export_model_to_nnef
+
+
+class DynLlamaRotaryEmbedding(torch.nn.Module):
+    """LlamaRotaryEmbedding without 'orignal caching'.
+
+    Caching is hurtfull in this case because
+    when torch jit trace is applied static max sequence seq_length
+    is done but we do not know what will be size of generation after NNEF export
+
+    """
+
+    def __init__(
+        self, dim, max_position_embeddings=2048, base=10000, device=None
+    ):
+        super().__init__()
+
+        self.dim = dim
+        self.max_position_embeddings = max_position_embeddings
+        self.base = base
+        inv_freq = 1.0 / (
+            self.base
+            ** (torch.arange(0, self.dim, 2).float().to(device) / self.dim)
+        )
+        self.register_buffer("inv_freq", inv_freq, persistent=False)
+
+    def forward(self, x, seq_len=None):
+        # _, _, seq_len_local, _ = x.shape
+        # x: [bs, num_attention_heads, seq_len, head_size]
+        if isinstance(seq_len, torch.Tensor):
+            seq_len = (
+                seq_len.tolist()
+            )  # fix issue with jit trace and tensor seq_len
+        t = torch.arange(seq_len).to(self.inv_freq.dtype)
+        freqs = torch.einsum("i,j->ij", t, self.inv_freq)
+        # freqs = t.unsqueeze(0).T @ self.inv_freq.unsqueeze(0)
+
+        # Different from paper, but it uses a different permutation in order to obtain the same calculation
+        emb = torch.cat((freqs, freqs), dim=-1)
+        return (emb.cos(), emb.sin())
+
+
+modeling_llama.LlamaRotaryEmbedding = DynLlamaRotaryEmbedding
 
 
 class SuperBasicCausal(torch.nn.Module):
     def __init__(self, model: LlamaForCausalLM):
         super().__init__()
         self.model = model
-        self.softmax = torch.nn.Softmax()
 
     def forward(self, input_ids: torch.Tensor):
         """same as calling without any smart caching mechanism self.model.model+lm_head and softmax.
@@ -60,7 +103,7 @@ class SuperBasicCausal(torch.nn.Module):
             )
             hidden_states = layer_outputs[0]
         logits = self.model.lm_head(hidden_states)
-        return self.softmax(logits)
+        return logits
 
 
 class Llama2SLugs(str, Enum):
@@ -88,11 +131,20 @@ def parser_cli():
 
 def main():
     args = parser_cli()
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
     default_model_slug = Llama2SLugs(args.model_slug).value  # check enum type
     tokenizer = AutoTokenizer.from_pretrained(default_model_slug)
+    # NOTE: size of tokenized text need to be very large because of logic inside
+    # modeling_llama2 rotary logic that use cache system not JITABLE based on seq len ...
     test_input = tokenizer("Hello, I am happy", return_tensors="pt")
     causal_llama = AutoModelForCausalLM.from_pretrained(default_model_slug)
     striped_model = SuperBasicCausal(causal_llama)
+
+    generated_ids = causal_llama.generate(**test_input)
+    print(generated_ids)
+    print(tokenizer.batch_decode(generated_ids, skip_special_tokens=True))
+    caus_res = striped_model(test_input.input_ids)
+    print("caus_res.shape:", caus_res.shape)
 
     export_model_to_nnef(
         model=striped_model,
@@ -103,6 +155,7 @@ def main():
         log_level=log.INFO,
         check_same_io_as_tract=True,
         dynamic_axes={"input_ids": {1: "S"}},
+        renaming_scheme="natural_verbose",
     )
 
 
