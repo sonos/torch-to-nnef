67e97e08e09d89a2836a89dbc1e90d3f293f65ee
Julien Balian <julien.balian@sonos.com>
2024-07-09 18:20:06 +0200

    fix: math case

--- torch_to_nnef/op/primitive/math.py
+++ torch_to_nnef/op/primitive/math.py
@@ -220,8 +220,9 @@ def mul(g, node, name_to_tensor, **kwargs):
         if isinstance(c_node, PythonConstant):
             # because torch.ops.aten.mul(float, tensor(float)) give complex number
             c_node = c_node.into_tensor_variable()
-        elif any(
-            nod.dtype.is_floating_point for nod in [input_node, other_node]
+        if any(
+            not isinstance(nod, PythonConstant) and nod.dtype.is_floating_point
+            for nod in [input_node, other_node]
         ):
             c_node.cast_float_inplace()
         inputs.append(
