726ace201664e8beddb72dd2aa91e82cdb4eeb83
Julien Balian <julien.balian@sonos.com>
2024-09-23 15:32:25 +0200

    feat: bump support to tract 0.21.7

--- torch_to_nnef/inference_target/tract.py
+++ torch_to_nnef/inference_target/tract.py
@@ -54,7 +54,7 @@ class TractNNEF(InferenceTarget):
     OFFICIAL_SUPPORTED_VERSIONS = [
         SemanticVersion.from_str(version)
         for version in [
-            "0.21.6",
+            "0.21.7",
             "0.20.22",
             "0.19.16",
         ]
