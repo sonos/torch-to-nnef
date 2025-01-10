ac51162b97d0e87c86014aa48b06f9ae7e8d2c20
Julien Balian <julien.balian@sonos.com>
2025-01-10 14:33:28 +0100

    fix: phi spda class moved in last huggingface transformers lib version

--- tests/test_phi.py
+++ tests/test_phi.py
@@ -54,48 +54,52 @@ class DummyModel(nn.Module):
         )
 
 
-@pytest.mark.parametrize(
-    "inference_target",
-    [_ for _ in TRACT_INFERENCES_TO_TESTS if _.version >= "0.21.4"],
-)
-def test_phi_spda_attn(inference_target):
-    mod = DummyModel()
-    S = 6
-    P = 10
-    with torch.no_grad():
-        test_input = (
-            torch.rand(1, S, 2048),
-            torch.rand(1, 32, P, 64),
-            torch.rand(1, 32, P, 64),
-        )
-        with tempfile.TemporaryDirectory() as tmpdir:
-            export_path = Path(tmpdir) / "model.nnef"
+if hasattr(
+    modeling_phi, "PhiSdpaAttention"
+):  # new version of huggingface doesn't have it
 
-            model = mod.eval()
-
-            input_names = ["hidden_states", "past_keys", "past_values"]
-            output_names = [
-                "final_attn_output",
-                "new_past_keys",
-                "new_past_values",
-            ]
-            dbg_name = datetime.now().strftime("%Y_%m_%dT%H_%M_%S")
-            dbg_name = f"{dbg_name}_phi_spda"
-            inference_target = deepcopy(inference_target)
-            inference_target.dynamic_axes = {
-                "hidden_states": {1: "S"},
-                "past_keys": {2: "P"},
-                "past_values": {2: "P"},
-            }
-            export_model_to_nnef(
-                model=model,
-                args=test_input,
-                file_path_export=export_path,
-                input_names=input_names,
-                output_names=output_names,
-                # log_level=log.INFO,
-                inference_target=inference_target,
-                debug_bundle_path=(Path.cwd() / "failed_tests" / dbg_name)
-                if os.environ.get("DEBUG", False)
-                else None,
+    @pytest.mark.parametrize(
+        "inference_target",
+        [_ for _ in TRACT_INFERENCES_TO_TESTS if _.version >= "0.21.4"],
+    )
+    def test_phi_spda_attn(inference_target):
+        mod = DummyModel()
+        S = 6
+        P = 10
+        with torch.no_grad():
+            test_input = (
+                torch.rand(1, S, 2048),
+                torch.rand(1, 32, P, 64),
+                torch.rand(1, 32, P, 64),
             )
+            with tempfile.TemporaryDirectory() as tmpdir:
+                export_path = Path(tmpdir) / "model.nnef"
+
+                model = mod.eval()
+
+                input_names = ["hidden_states", "past_keys", "past_values"]
+                output_names = [
+                    "final_attn_output",
+                    "new_past_keys",
+                    "new_past_values",
+                ]
+                dbg_name = datetime.now().strftime("%Y_%m_%dT%H_%M_%S")
+                dbg_name = f"{dbg_name}_phi_spda"
+                inference_target = deepcopy(inference_target)
+                inference_target.dynamic_axes = {
+                    "hidden_states": {1: "S"},
+                    "past_keys": {2: "P"},
+                    "past_values": {2: "P"},
+                }
+                export_model_to_nnef(
+                    model=model,
+                    args=test_input,
+                    file_path_export=export_path,
+                    input_names=input_names,
+                    output_names=output_names,
+                    # log_level=log.INFO,
+                    inference_target=inference_target,
+                    debug_bundle_path=(Path.cwd() / "failed_tests" / dbg_name)
+                    if os.environ.get("DEBUG", False)
+                    else None,
+                )
