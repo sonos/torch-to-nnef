3ceab7b849211225185d35179d4de5737b6d12a1
Julien Balian <julien.balian@sonos.com>
2025-02-10 17:46:15 +0100

    Bump version: 0.15.8 → 0.15.9

--- torch_to_nnef/__init__.py
+++ torch_to_nnef/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Julien Balian"""
 __email__ = "julien.balian@sonos.com"
-__version__ = "0.15.8"
+__version__ = "0.15.9"
 
 from torch_to_nnef.export import (
     export_model_to_nnef,
