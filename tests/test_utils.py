7bf8fad758d499cc8f6e39b86ab6530882d2ad42
Julien Balian <julien.balian@sonos.com>
2025-06-17 15:42:57 +0200

    feat: base param updater

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,8 +2,13 @@ from dataclasses import dataclass
 
 import pytest
 
+import torch
+from torch import nn
+
+from torch_to_nnef.exceptions import InconsistentTensorError
 from torch_to_nnef.utils import (
     NamedItem,
+    ParametersModelUpdater,
     ReactiveNamedItemDict,
     flatten_dict_tuple_or_list,
 )
@@ -116,3 +121,146 @@ _add_flatten_example(
 def test_flatten(inputs, outputs):
     gen_outs = flatten_dict_tuple_or_list(inputs)
     assert gen_outs == outputs
+
+
+class CustomMod(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.seq = nn.Sequential(
+            nn.Conv1d(3, 10, 3),
+            nn.ReLU(),
+            nn.BatchNorm1d(10),
+            nn.Conv1d(10, 10, 1),
+            nn.ReLU(),
+            nn.BatchNorm1d(10),
+            nn.Conv1d(10, 10, 1),
+            nn.BatchNorm1d(10),
+            nn.Conv1d(10, 3, 1),
+            nn.ReLU(),
+        )
+        self.last_lin_proj = nn.Linear(3, 5)
+        self.embedding = nn.Embedding(5, 3)
+
+    def forward(self, inp: torch.Tensor):
+        x = self.embedding(inp)
+        x = x.permute(0, 2, 1)
+        x = self.seq(x)
+        x = x.permute(0, 2, 1)
+        x = self.last_lin_proj(x)
+        return x
+
+
+def test_parameter_update_by_ref_with_tied():
+    model = CustomMod()
+    model.last_lin_proj.weight = model.embedding.weight  # Tied parameters
+    sample0 = torch.tensor([[1, 3, 4, 0, 1, 2]])
+    _ = model(sample0)
+    param_updaters = ParametersModelUpdater(model)
+    old_data = model.last_lin_proj.weight.clone()
+    assert id(model.last_lin_proj.weight) == id(model.embedding.weight)
+    param_updaters.update_by_ref(
+        ref=model.embedding.weight,
+        new_tensor=torch.arange(5 * 3).reshape(5, 3).float(),
+    )
+    assert id(model.last_lin_proj.weight) == id(model.embedding.weight)
+    assert not torch.eq(old_data, model.last_lin_proj.weight).all()
+    assert torch.eq(
+        torch.arange(5 * 3).reshape(5, 3).float(), model.last_lin_proj.weight
+    ).all()
+
+
+def test_parameter_update_by_name_with_tied():
+    model = CustomMod()
+    model.last_lin_proj.weight = model.embedding.weight  # Tied parameters
+    sample0 = torch.tensor([[1, 3, 4, 0, 1, 2]])
+    _ = model(sample0)
+    param_updaters = ParametersModelUpdater(model)
+
+    old_data = model.last_lin_proj.weight.clone()
+    assert id(model.last_lin_proj.weight) == id(model.embedding.weight)
+
+    new_tensor = torch.arange(5 * 3).reshape(5, 3).float()
+    param_updaters.update_by_name(
+        name="embedding.weight", new_tensor=new_tensor
+    )
+
+    assert id(model.last_lin_proj.weight) == id(model.embedding.weight)
+    assert not torch.eq(old_data, model.last_lin_proj.weight).all()
+    assert torch.eq(new_tensor, model.last_lin_proj.weight).all()
+
+
+def test_parameter_update_by_name_with_tied_modified_separatly():
+    model = CustomMod()
+    model.last_lin_proj.weight = model.embedding.weight  # Tied parameters
+    sample0 = torch.tensor([[1, 3, 4, 0, 1, 2]])
+    _ = model(sample0)
+    param_updaters = ParametersModelUpdater(model)
+
+    old_data = model.last_lin_proj.weight.clone()
+    assert id(model.last_lin_proj.weight) == id(model.embedding.weight)
+
+    new_tensor = torch.arange(5 * 3).reshape(5, 3).float()
+    param_updaters.update_by_name(
+        name="embedding.weight", new_tensor=new_tensor, tie_replacements=False
+    )
+
+    assert id(model.last_lin_proj.weight) != id(model.embedding.weight)
+    assert torch.eq(old_data, model.last_lin_proj.weight).all()
+    assert not torch.eq(new_tensor, model.last_lin_proj.weight).all()
+
+
+def test_parameter_update_by_name_with_tied_modified_separatly():
+    model = CustomMod()
+    model.last_lin_proj.weight = model.embedding.weight  # Tied parameters
+    sample0 = torch.tensor([[1, 3, 4, 0, 1, 2]])
+    _ = model(sample0)
+    param_updaters = ParametersModelUpdater(model)
+    assert id(model.last_lin_proj.weight) == id(model.embedding.weight)
+
+    new_tensor = torch.arange(5 * 2).reshape(5, 2).float()
+    with pytest.raises(InconsistentTensorError):
+        param_updaters.update_by_name(
+            name="embedding.weight",
+            new_tensor=new_tensor,
+        )
+
+    new_tensor = torch.arange(5 * 3).reshape(5, 3).half()
+    with pytest.raises(InconsistentTensorError):
+        param_updaters.update_by_name(
+            name="embedding.weight",
+            new_tensor=new_tensor,
+        )
+
+    new_tensor = torch.arange(5 * 3).reshape(5, 3).float().to("meta")
+    with pytest.raises(InconsistentTensorError):
+        param_updaters.update_by_name(
+            name="embedding.weight",
+            new_tensor=new_tensor,
+        )
+    new_tensor = torch.arange(5 * 3).reshape(5, 3).float().to("meta")
+    param_updaters.update_by_name(
+        name="embedding.weight",
+        new_tensor=new_tensor,
+        enforce_same_shape_dtype_device=False,
+    )
+
+
+def test_add_parameter_if_unset():
+    model = CustomMod()
+    model.last_lin_proj.weight = model.embedding.weight  # Tied parameters
+    sample0 = torch.tensor([[1, 3, 4, 0, 1, 2]])
+    _ = model(sample0)
+    param_updaters = ParametersModelUpdater(model, add_parameter_if_unset=False)
+    assert id(model.last_lin_proj.weight) == id(model.embedding.weight)
+
+    new_tensor = torch.arange(5 * 3).reshape(5, 3).float()
+    with pytest.raises(ValueError):
+        param_updaters.update_by_name(
+            name="embedding.weight",
+            new_tensor=new_tensor,
+        )
+
+    param_updaters.update_by_name(
+        name="embedding.weight",
+        new_tensor=nn.Parameter(new_tensor),
+    )
--- torch_to_nnef/utils.py
+++ torch_to_nnef/utils.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import contextlib
 import functools
 import logging
@@ -12,6 +13,7 @@ from torch import _C
 
 from torch_to_nnef.exceptions import (
     DataNodeValueError,
+    InconsistentTensorError,
     TorchToNNEFNotImplementedError,
 )
 
@@ -360,6 +362,127 @@ def get_parent_module_and_param_name(
     for mod_name in chunked_names[:-1]:
         ref_mod = getattr(ref_mod, mod_name)
     return ref_mod, chunked_names[-1]
+
+
+class ParametersModelUpdater:
+    """Helper to update parameters of a model cleanly"""
+
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        add_parameter_if_unset=True,
+    ):
+        self.name_to_id = {}
+        id_to_names = defaultdict(set)
+        mod_name_to_param_names = defaultdict(list)
+        for param_name, param in model.named_parameters(remove_duplicate=False):
+            self.name_to_id[param_name] = id(param)
+            id_to_names[id(param)].add(param_name)
+            mod_name, _ = self.split_param_name(param_name)
+            mod_name_to_param_names[mod_name].append(param_name)
+        self.id_to_names = {k: frozenset(v) for k, v in id_to_names.items()}
+        self.name_to_parent_module = {}
+        for mod_name, mod in model.named_modules():
+            for p_name in mod_name_to_param_names[mod_name]:
+                self.name_to_parent_module[p_name] = mod
+        self.model = model
+        self.add_parameter_if_unset = add_parameter_if_unset
+
+    @staticmethod
+    def split_param_name(name: str) -> T.Tuple[str, str]:
+        if "." in name:
+            mod_name, param_local_name = name.rsplit(".", 1)
+        else:
+            mod_name = ""
+            param_local_name = name
+        return mod_name, param_local_name
+
+    def check_same_dtype(self, tensor0: torch.Tensor, tensor1: torch.Tensor):
+        if tensor0.dtype != tensor1.dtype:
+            raise InconsistentTensorError(
+                f"not same dtype between {tensor0.dtype} and {tensor1.dtype}"
+            )
+
+    def check_same_device(self, tensor0: torch.Tensor, tensor1: torch.Tensor):
+        if tensor0.device != tensor1.device:
+            raise InconsistentTensorError(
+                f"not same device between {tensor0.device} and {tensor1.device}"
+            )
+
+    def check_same_shape(self, tensor0: torch.Tensor, tensor1: torch.Tensor):
+        if tensor0.shape != tensor1.shape:
+            raise InconsistentTensorError(
+                f"not same shape between {tensor0.shape} and {tensor1.shape}"
+            )
+
+    def check_all(self, tensor0: torch.Tensor, tensor1: torch.Tensor):
+        self.check_same_device(tensor0, tensor1)
+        self.check_same_dtype(tensor0, tensor1)
+        self.check_same_shape(tensor0, tensor1)
+
+    def maybe_parameterize(self, new_tensor: torch.Tensor) -> torch.Tensor:
+        if not isinstance(new_tensor, torch.nn.Parameter):
+            if self.add_parameter_if_unset:
+                new_tensor = torch.nn.Parameter(new_tensor, requires_grad=False)
+            else:
+                raise ValueError(f"{new_tensor} must be a parameter")
+        return new_tensor
+
+    def update_by_ref(
+        self,
+        ref: torch.nn.Parameter,
+        new_tensor: torch.Tensor,
+        enforce_same_shape_dtype_device: bool = True,
+    ):
+        new_tensor = self.maybe_parameterize(new_tensor)
+        if enforce_same_shape_dtype_device:
+            self.check_all(ref, new_tensor)
+        need_grad = False
+        if ref.requires_grad:
+            need_grad = True
+            ref.requires_grad = False
+        ref.set_(new_tensor)
+        if need_grad and not ref.requires_grad:
+            ref.requires_grad = True
+
+    def update_by_name(
+        self,
+        name: str,
+        new_tensor: torch.Tensor,
+        tie_replacements: bool = True,
+        enforce_same_shape_dtype_device: bool = True,
+    ):
+        new_tensor = self.maybe_parameterize(new_tensor)
+        mod = self.name_to_parent_module[name]
+        _, p_local_name = self.split_param_name(name)
+        old = mod._parameters[p_local_name]
+        if enforce_same_shape_dtype_device:
+            self.check_all(old, new_tensor)
+        if tie_replacements:
+            need_grad = False
+            if old.requires_grad:
+                need_grad = True
+                old.requires_grad = False
+            if (
+                old.device == new_tensor.device
+                and old.dtype == new_tensor.dtype
+            ):
+                old.set_(new_tensor)
+            else:
+                # slower but allow device or dtype change
+                for n in self.id_to_names[id(old)]:
+                    self.name_to_parent_module[n]._parameters[
+                        self.split_param_name(n)[1]
+                    ] = new_tensor
+            if need_grad and not old.requires_grad:
+                old.requires_grad = True
+        else:
+            mod._parameters[p_local_name] = new_tensor
+            if (
+                old.requires_grad
+                and not mod._parameters[p_local_name].requires_grad
+            ):
+                mod._parameters[p_local_name].requires_grad = True
 
 
 class ReactiveNamedItemDict:
