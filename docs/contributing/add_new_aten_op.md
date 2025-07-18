d055f46ad2c4d50cafd83ef43f8479c0a7716045
Julien Balian <julien.balian@sonos.com>
2025-07-18 19:44:07 +0200

    fix: repport ops generator

--- docs/contributing/add_new_aten_op.md
+++ docs/contributing/add_new_aten_op.md
@@ -11,10 +11,13 @@
     - [ ] PyTorch and Python basics
     - [ ] 5 min to read this page
 
-There is a lot of operators in the PyTorch internal representation,
-and while the most common are handled correctly, some
-are still missing in `torch_to_nnef` due to the development of this library which
-is done on a per need basis (aka we need to exort a new model, ow it
-miss this and that operator let's implement it).
+There is a lot of operators in the PyTorch internal representation.
+[Aten](https://docs.pytorch.org/executorch/stable/ir-ops-set-definition.html) is the underling cpp namespace in which most of the PyTorch computational operators are specified.
+[Looking at the list](https://docs.pytorch.org/docs/main/torch.compiler_ir.html) in the PyTorch IR it may seems at first there is only: 191 ops available, (not accounting quantized & prims namespace) but digging  with few scripts we maintain a  [generated compatibility list](./supported_operators.md) that seems to show something different (~500 ops).
+
+While the most common are handled correctly, this list is ever expanding or an operator receive new arguments, so we alway need to catch-up when a new model end up adopting one of those.
+
+In the development of this library we did thing on a per need basis (aka we need to export a new model,
+ow it miss this and that operator let's implement it).
 
 In this tutorial we will share with you how to contribute a new operator:
---
+++ docs/contributing/get_torch_ir_names.py
@@ -0,0 +1,111 @@
+from datetime import datetime
+from pathlib import Path
+from torch_to_nnef.op.aten import aten_ops_registry
+import requests as rq
+import bs4
+import subprocess
+
+
+TORCH_VERSION = "v2.7.1"
+URL_IR = "https://docs.pytorch.org/docs/main/torch.compiler_ir.html"
+resp = rq.get(URL_IR)
+
+assert resp.status_code == 200
+
+soup = bs4.BeautifulSoup(resp.content, "html.parser")
+
+res = soup.find_all("span", {"class": "pre"})
+official_aten_names = set(
+    [
+        r.text.split(".")[1]
+        for r in res
+        if r.text.startswith("aten")
+        if "backward" not in r.text
+    ]
+)
+official_prim_names = sorted(
+    [r.text.split(".")[1] for r in res if r.text.startswith("prim")]
+)
+
+t2n_aten = set(list(aten_ops_registry._registry.keys()))
+
+aten_torch_from_code = sorted(
+    subprocess.check_output(
+        "cd /tmp ; "
+        "git -C 'pytorch' pull || git clone -q git@github.com:pytorch/pytorch.git; "
+        "cd /tmp/pytorch ;"
+        f"git checkout {TORCH_VERSION}; "
+        'rg "aten::" | sed "s|.*aten::\\([a-zA-Z0-9_]*\\).*|\\1|g"|sort|uniq',
+        shell=True,
+    )
+    .decode("utf8")
+    .split()
+)
+aten_torch_from_code = [
+    _ for _ in aten_torch_from_code if not _.startswith("_")
+]
+
+support_inplace = set()
+offset = 0
+for ix, a in enumerate(aten_torch_from_code[:]):
+    if a.endswith("_") and a[:-1] in aten_torch_from_code:
+        del aten_torch_from_code[ix - offset]
+        offset += 1
+        support_inplace.add(a[:-1])
+
+
+matched_qte = 0
+with (Path(__file__).parent / "./supported_operators.md").open(
+    "w", encoding="utf8"
+) as fh:
+    date = datetime.now().strftime("%d %b %Y")
+    print(
+        "!!! note\n"
+        f"    This list and file are auto generated from 'a script' that dig into PyTorch."
+        f" Version targetted is:  **'{TORCH_VERSION}'**. file was generated the **{date}**\n"
+        "\n\n"
+        f"\n Also 'is core' column refers to this [pytorch documentation page]({URL_IR})",
+        file=fh,
+    )
+    rows = []
+    for a_from_code in aten_torch_from_code:
+        is_core = a_from_code in official_aten_names
+        is_core_official_str = "✅" if is_core else "-"
+
+        exist_in_t2n = a_from_code in t2n_aten
+
+        mapped_in_t2n_str = "✅" if exist_in_t2n else "❌"
+        if exist_in_t2n:
+            matched_qte += 1
+
+        inplace_str = "✅" if a_from_code in support_inplace else "❌"
+
+        rows.append(
+            (
+                f"| {a_from_code} | | {inplace_str} | {is_core_official_str} | {mapped_in_t2n_str} |",
+                is_core,
+            )
+        )
+    rows = sorted(rows, key=lambda x: -int(x[1]))
+    print("", file=fh)
+    t2n_n_ops = len([_ for _ in t2n_aten if not _.endswith("_")])
+    print(
+        "Total matched operators in `torch_to_nnef`: "
+        f"{matched_qte}/{len(aten_torch_from_code)} "
+        "while torch_to_nnef has a quantity of registered aten "
+        f"operators={t2n_n_ops}",
+        file=fh,
+    )
+    print("", file=fh)
+    print(
+        "| aten name | aliases | in place | is core | t2n translated |",
+        file=fh,
+    )
+    print(
+        "| -------- | ------- | ------- | --------- | ---------------- |",
+        file=fh,
+    )
+    for r in rows:
+        print(r[0], file=fh)
+
+    print("", file=fh)
