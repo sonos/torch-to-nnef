d2d4cfcff3ed56b18300b4855c7b4fa6578b7fa4
Julien Balian <julien.balian@sonos.com>
2025-08-11 12:28:27 +0200

    fix: better buffer + recalib better post proc vad

--- docs/examples/vad/src/lib.rs
+++ docs/examples/vad/src/lib.rs
@@ -6,10 +6,13 @@ use wasm_bindgen::prelude::*;
 
 extern crate web_sys;
 
+const AUDIO_BUFFER_SIZE: usize = (160.0 * 6.3) as usize;
+
 #[wasm_bindgen]
 struct VadClassifier {
     encoder_model: TypedRunnableModel<TypedModel>,
     decoder_model: TypedRunnableModel<TypedModel>,
+    audio_buffer: [f32; AUDIO_BUFFER_SIZE],
 }
 
 #[wasm_bindgen]
@@ -34,16 +37,24 @@ impl VadClassifier {
         Ok(VadClassifier {
             encoder_model,
             decoder_model,
+            audio_buffer: [0.0; AUDIO_BUFFER_SIZE],
         })
     }
 
-    fn predict_speech_presence_internal(&self, raw_audio_data: Vec<f32>) -> TractResult<f32> {
+    fn predict_speech_presence_internal(&mut self, raw_audio_data: Vec<f32>) -> TractResult<f32> {
         // web_sys::console::debug_1(&"start predict voice presence".into());
-
-        // prep audio data
-        let nd_audio_data = Array1::from_vec(raw_audio_data)
+        assert!(self.audio_buffer.len() > raw_audio_data.len());
+        // prep audio data {
+        // roll data
+        let start = self.audio_buffer.len() - raw_audio_data.len();
+        let end = self.audio_buffer.len();
+        self.audio_buffer.copy_within(start..end, 0);
+        // add fresh data
+        self.audio_buffer[..raw_audio_data.len()].copy_from_slice(&raw_audio_data);
+        let nd_audio_data = Array1::from_vec(self.audio_buffer.to_vec())
             .insert_axis(Axis(0))
             .into_tvalue();
+        // }
         // run the model on the input
         let enc_result = self.encoder_model.run(tvec!(nd_audio_data))?;
         let dec_result = self.decoder_model.run(enc_result)?;
@@ -52,11 +63,11 @@ impl VadClassifier {
         let score: ArrayView2<f32> = dec_result[0]
             .to_array_view::<f32>()?
             .into_dimensionality()?;
-        Ok(*score.get((0, 0)).unwrap())
+        Ok(*score.get((0, 1)).unwrap())
     }
 
     pub fn predict_speech_presence(
-        &self,
+        &mut self,
         js_raw_audio_data: &web_sys::js_sys::Float32Array,
     ) -> Result<JsValue, JsError> {
         let prediction_res = self.predict_speech_presence_internal(js_raw_audio_data.to_vec());
