8fabb4943fcbc9fafb9520974c09b7a18f5270a6
Julien Balian <julien.balian@sonos.com>
2025-08-12 11:23:13 +0200

    fix: better doc build

--- docs/examples/vad/run.sh
+++ docs/examples/vad/run.sh
@@ -1,2 +1,7 @@
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+python3 ./export.py
 wasm-pack build --target web --out-dir ../../html
-rm ../../html/.gitignore ../../html/*.ts ../../html/*.json
+rm ../../html/.gitignore ../../html/*.ts
+find ../../html/*.json -maxdepth 1 -type f -name '*.json' ! -name '1kclass.json' -delete
--- setup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-# This is a shim to hopefully allow Github to detect the package, build is done with uv
-
-import setuptools
-
-if __name__ == "__main__":
-    setuptools.setup(name="torch_to_nnef")
