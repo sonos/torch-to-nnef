dc7f298bf85f5a887a86124a106e43e1f750fc5e
Julien Balian <julien.balian@sonos.com>
2026-01-05 16:37:36 +0100

    fix: documentation on small screens: menu do not display all logo and hide subtitle

--- docs/html/sync_sender_colorscheme.js
+++ docs/html/sync_sender_colorscheme.js
@@ -1,9 +1,33 @@
+const updateImageColorsheme = () => {
+    const isSmallScreen = window.innerWidth < 1280;
+    const images = document.querySelectorAll('.logo-img');
+    const color = localStorage.getItem('colorscheme');
+    images.forEach(img => {
+        const isDarkImage = img.src.includes('#only-dark');
+        // case of default theme is white is not handled well
+        const shouldHide = isSmallScreen && (
+            (!isDarkImage && color === "black") ||
+            (isDarkImage && color !== "black")
+        );
+        img.style.display = shouldHide ? 'none' : '';
+    });
+    const menuSubtitle = document.querySelector(".md-nav__title");
+    if (isSmallScreen) {
+        menuSubtitle.removeChild(menuSubtitle.childNodes[2]);
+        menuSubtitle.style.height = "auto";
+    }
+}
 const updateColorscheme = () => {
     var colorscheme = document.body.getAttribute("data-md-color-primary");
     if (colorscheme) {
         console.log("Colorscheme changed:", colorscheme);
         localStorage.setItem("colorscheme", colorscheme);
+        updateImageColorsheme();
     }
 };
 document.body.addEventListener("change", updateColorscheme);
 document.addEventListener("DOMContentLoaded", updateColorscheme);
+
+updateImageColorsheme();
+window.addEventListener('resize', updateImageColorsheme);
+
