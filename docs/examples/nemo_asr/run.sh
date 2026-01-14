cb620b468a0eb4a195a47b9408138cce05fdfcb8
Julien Balian <julien.balian@sonos.com>
2026-01-14 19:34:30 +0100

    fix: run.sh used for now as demo of translation

--- docs/examples/nemo_asr/run.sh
+++ docs/examples/nemo_asr/run.sh
@@ -4,6 +4,7 @@ source .venv/bin/activate
 wget https://dldata-public.s3.us-east-2.amazonaws.com/2086-149220-0033.wav
 pip install -e ../../../[nemo-tract]
 t2n_export_nemo -s nvidia/parakeet-tdt-0.6b-v3 -e model --tract-specific-path $HOME/SONOS/src/tract/target/release/tract
-wasm-pack build --target web --out-dir ../../html
-rm ../../html/.gitignore ../../html/*.ts
-find ../../html/*.json -maxdepth 1 -type f -name '*.json' ! -name '1kclass.json' -delete
+cargo test -- --nocapture
+# wasm-pack build --target web --out-dir ../../html
+# rm ../../html/.gitignore ../../html/*.ts
+# find ../../html/*.json -maxdepth 1 -type f -name '*.json' ! -name '1kclass.json' -delete
