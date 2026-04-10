b0c9e050206fd65d4f7459002da6071d2df77fd0
Julien Balian <julien.balian@sonos.com>
2026-04-10 23:18:12 +0200

    fix: add strict=True to zip in expand_input_names (ruff B905)

--- packages/nemo-asr/torch_to_nnef_nemo/dynaxes.py
+++ packages/nemo-asr/torch_to_nnef_nemo/dynaxes.py
@@ -41,7 +41,7 @@ def expand_input_names(
         # Truncate to example length: NeMo >= 2.6 may list optional inputs
         # (e.g. bypass_pre_encode) that have no corresponding example tensor.
         input_names = input_names[: len(input_example)]
-        for name, val in zip(input_names, input_example):
+        for name, val in zip(input_names, input_example, strict=True):
             if isinstance(val, (list, tuple)) and len(val) > 0:
                 tnames: T.List[str] = []
                 for i, elem in enumerate(val):
