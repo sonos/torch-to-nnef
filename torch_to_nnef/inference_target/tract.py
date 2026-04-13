d825f1ee5f806d7a686c122a90888b7fd29f334a
Julien Balian <julien.balian@sonos.com>
2026-04-13 13:38:50 +0200

    fix: update SDPA test for tract 0.22.1 reify_sdpa_operator, bump tract to 0.22.1

--- torch_to_nnef/inference_target/tract.py
+++ torch_to_nnef/inference_target/tract.py
@@ -81,7 +81,7 @@ class TractNNEF(InferenceTarget):
     OFFICIAL_SUPPORTED_VERSIONS = [
         SemanticVersion.from_str(version)
         for version in [
-            "0.22.0",
+            "0.22.1",
             "0.21.15",
         ]
     ]
